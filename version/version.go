f6c27efa3c6435bdf0422dcb8af8a57359c1267e
Loki Verloren <stalker.loki@protonmail.ch>
2021-04-30 18:22:15 +0200

    updating version

--- version/version.go
+++ version/version.go
@@ -11,14 +11,14 @@ var (
 	// URL is the git URL for the repository
 	URL = "github.com/p9c/gel"
 	// GitRef is the gitref, as in refs/heads/branchname
-	GitRef = "HEAD"
+	GitRef = "refs/heads/master"
 	// GitCommit is the commit hash of the current HEAD
-	GitCommit = "e3bfce4ba69afe391a2979f922559eaa573f0b97"
+	GitCommit = "e5e1311849f688c05fb81ae2ab5aedd5833f2ee6"
 	// BuildTime stores the time when the current binary was built
-	BuildTime = "2021-04-30T18:12:27+02:00"
+	BuildTime = "2021-04-30T18:21:48+02:00"
 	// Tag lists the Tag on the build, adding a + to the newest Tag if the commit is
 	// not that commit
-	Tag = "v0.1.20"
+	Tag = "v0.1.21"
 	// PathBase is the path base returned from runtime caller
 	PathBase = "/home/loki/src/github.com/p9c/pod/pkg/gel/"
 	// Major is the major number from the tag
@@ -26,7 +26,7 @@ var (
 	// Minor is the minor number from the tag
 	Minor = 1
 	// Patch is the patch version number from the tag
-	Patch = 20
+	Patch = 21
 	// Meta is the extra arbitrary string field from Semver spec
 	Meta = ""
 )
